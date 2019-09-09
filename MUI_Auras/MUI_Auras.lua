b0f038fc43a2c7e8aebada3b3fbda3c0e3736080
Mayron <mjr_4077@hotmail.co.uk>
2019-09-09 07:00:45 +0100

    testing

--- MUI_Auras/MUI_Auras.lua
+++ MUI_Auras/MUI_Auras.lua
@@ -1,19 +1,16 @@
 -- luacheck: ignore self 143
-local _, namespace = ...;
-
 local _G, MayronUI = _G, _G.MayronUI;
 local tk, db, em, _, obj = MayronUI:GetCoreComponents();
 
 local GetTime, SecondsToTimeAbbrev, GetWeaponEnchantInfo, select, UnitAura, pairs, ipairs,
-    table, tostring, CreateFrame, GameTooltip, unpack, math, GetInventoryItemTexture, string,
-    BUFF_MAX_DISPLAY, DEBUFF_MAX_DISPLAY, UIParent =
-    _G.GetTime, _G.SecondsToTimeAbbrev, _G.GetWeaponEnchantInfo, _G.select, _G.UnitAura,
-    _G.pairs, _G.ipairs, _G.table, _G.tostring, _G.CreateFrame, _G.GameTooltip, _G.unpack,
-    _G.math, _G.GetInventoryItemTexture, _G.string, _G.BUFF_MAX_DISPLAY, _G.DEBUFF_MAX_DISPLAY, _G.UIParent;
+    CreateFrame, GameTooltip, unpack, math, GetInventoryItemTexture, string, BUFF_MAX_DISPLAY,
+    DEBUFF_MAX_DISPLAY, UIParent = _G.GetTime, _G.SecondsToTimeAbbrev, _G.GetWeaponEnchantInfo,
+    _G.select, _G.UnitAura, _G.pairs, _G.ipairs, _G.CreateFrame, _G.GameTooltip, _G.unpack, _G.math,
+    _G.GetInventoryItemTexture, _G.string, _G.BUFF_MAX_DISPLAY, _G.DEBUFF_MAX_DISPLAY, _G.UIParent;
 
-local MAIN_ENCHANT_ID = 1016;
-local OFF_ENCHANT_ID = 1017;
-local framesToUpdate = {};
+-- Main-Hand, Off-Hand, Ranged
+local enchantAuraIds = { 16, 17, 18 };
+local ARGS_PER_ITEM = 4;
 
 -- Objects -----------------------------
 
@@ -24,14 +21,9 @@ local Engine = obj:Import("MayronUI.Engine");
 
 ---@class AurasModule : BaseModule
 local C_AurasModule = MayronUI:RegisterModule("AurasModule", "Auras (Buffs & Debuffs)");
-namespace.C_AurasModule = C_AurasModule;
-
----@class C_Aura : Object
-local C_Aura = Engine:CreateClass("Aura", "Framework.System.FrameWrapper");
 
 ---@class C_AuraArea : Object
 local C_AuraArea = Engine:CreateClass("AuraArea", "Framework.System.FrameWrapper");
-C_AuraArea.Static:AddFriendClass("Aura");
 
 -- Load Database Defaults --------------
 
@@ -64,81 +56,11 @@ db:AddToDefaults("profile.auras", {
     };
 });
 
--- C_Aura ----------------------
-local function AuraFrame_OnEnter(self)
-    GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT", 0, 0);
+-- Local Functions -------------
 
-    if (not self.filter) then
-        GameTooltip:SetInventoryItem("player", self:GetID() - 1000);
-    else
-        GameTooltip:SetUnitAura("player", self:GetID(), self.filter);
-    end
-
-    GameTooltip:Show();
-end
-
--- Engine:DefineParams("table", "table", "string");
-function C_Aura:SetArea(data, auraArea)
-    local areaData = data:GetFriendData(auraArea);
-    data.frame:SetParent(areaData.frame);
-    data.areaFrame = areaData.frame;
-    data.auraType = areaData.settings.auraType;
-
-    if (data.auraType == "Buffs") then
-        data.frame:SetAttribute("type2", "cancelaura");
-        data.frame:SetAttribute("cancelaura", "player", data.frame:GetID());
-        data.frame.filter = "HELPFUL";
-    else
-        data.frame:SetAttribute("type2", nil);
-        data.frame:SetAttribute("cancelaura", nil);
-        data.frame.filter = "HARMFUL";
-    end
-
-    table.insert(framesToUpdate[tostring(areaData.frame)], data.frame);
-end
-
-function C_Aura:GetAreaFrame(data)
-    return data.areaFrame;
-end
-
--- TODO: Need to use the Stack object to create these!
-function C_Aura:__Construct(data, auraArea, id)
-    data.backdrop = obj:PopTable();
-
-    local btn = CreateFrame("Button", nil, nil, "SecureActionButtonTemplate");
-    btn:SetID(id);
-    btn:RegisterForClicks("RightButtonUp");
-
-    data.frame = btn; -- needed for GetFrame()
-    self:SetArea(auraArea);
-
-    btn:SetScript("OnEnter", AuraFrame_OnEnter);
-    btn:SetScript("OnLeave", tk.GeneralTooltip_OnLeave);
-
-    -- SHOULD be in UpdateAppearance (called by auraArea)
-    btn:SetSize(32, 32);
-
-    if (id == MAIN_ENCHANT_ID or id == OFF_ENCHANT_ID) then
-        btn.background = tk:SetBackground(btn, 0.53, 0.23, 0.78);
-    else
-        btn.background = tk:SetBackground(btn, 0, 0, 0);
-    end
-
-    btn.iconTexture = btn:CreateTexture(nil, "ARTWORK");
-    btn.iconTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-    btn.iconTexture:SetPoint("TOPLEFT", 1, -1);
-    btn.iconTexture:SetPoint("BOTTOMRIGHT", -1, 1);
-
-    btn.countText = btn:CreateFontString(nil, "OVERLAY", "NumberFontNormal");
-    btn.countText:SetPoint("BOTTOMRIGHT", 0, 2);
-
-    btn.timeRemainingText = btn:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall");
-    btn.timeRemainingText:SetPoint("TOP", btn, "BOTTOM", 0, -2);
-end
-
-local function AuraFrame_OnUpdate(self)
+local function AuraButton_OnUpdate(self)
     local _, _, count, _, _, expirationTime, _, _, _,
-        _, _, _, _, _, timeMod = UnitAura("player", self:GetID(), "HELPFUL");
+        _, _, _, _, _, timeMod = UnitAura("player", self.id, "HELPFUL");
 
     if (not (count and expirationTime)) then
         return;
@@ -165,26 +87,17 @@ local function AuraFrame_OnUpdate(self)
     end
 end
 
-local function AuraEnchantFrame_OnUpdate(self, globalName)
-    local id = self:GetID();
-    local hasEnchant, expirationTime, count;
-
-    if (id == MAIN_ENCHANT_ID) then
-        hasEnchant, expirationTime, count = GetWeaponEnchantInfo();
-    else
-        hasEnchant, expirationTime, count = select(5, GetWeaponEnchantInfo());
-    end
-
-    self.filter = nil; -- weapon enchant
+local function AuraEnchantButton_OnUpdate(self, globalName)
+    local id = self.id;
+    local index = tk.Tables:IndexOf(enchantAuraIds, id);
+    local hasEnchant, expirationTime, count = select(ARGS_PER_ITEM * (index - 1) + 1, GetWeaponEnchantInfo());
 
     if (not hasEnchant) then
         self.isEnchantActive = nil;
+        -- enable/disable auraButtons:
         em:FindEventHandlerByKey(globalName.."Handler"):Run("UNIT_AURA");
         return;
     end
-
-    local iconTexture = GetInventoryItemTexture("player", MAIN_ENCHANT_ID - 1000);
-    self.iconTexture:SetTexture(iconTexture);
 
     if (count < 1) then
         self.countText:SetText(tk.Strings.Empty);
@@ -204,24 +117,117 @@ local function AuraEnchantFrame_OnUpdate(self, globalName)
     -- ensure that PLAYER_ENTERING_WORLD works for enchants
     if (not self.isEnchantActive) then
         self.isEnchantActive = true;
+        -- enable/disable auraButtons:
         em:FindEventHandlerByKey(globalName.."Handler"):Run("UNIT_AURA");
     end
 end
 
-function C_Aura:Enable(data, iconTexture)
-    data.frame:EnableMouse(true);
-    data.frame.iconTexture:SetTexture(iconTexture);
-    data.frame.background:SetVertexColor(0, 0, 0, 1);
+local function AuraButton_OnEnter(self)
+    GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT", 0, 0);
+
+    if (not self.filter) then
+        GameTooltip:SetInventoryItem("player", self.id);
+    else
+        GameTooltip:SetUnitAura("player", self.id, self.filter);
+    end
+
+    GameTooltip:Show();
 end
 
-function C_Aura:UpdateAppearance()
+local function CreateAuraButton(parent, filter)
+    local btn = CreateFrame("Button", nil, parent, "SecureActionButtonTemplate");
+    btn:RegisterForClicks("RightButtonUp");
+    btn:SetScript("OnEnter", AuraButton_OnEnter);
+    btn:SetScript("OnLeave", tk.GeneralTooltip_OnLeave);
 
+    btn:SetSize(32, 32);
+
+    btn.filter = filter;
+
+    btn.iconTexture = btn:CreateTexture(nil, "ARTWORK");
+    btn.iconTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+    btn.iconTexture:SetPoint("TOPLEFT", 1, -1);
+    btn.iconTexture:SetPoint("BOTTOMRIGHT", -1, 1);
+
+    btn.countText = btn:CreateFontString(nil, "OVERLAY", "NumberFontNormal");
+    btn.countText:SetPoint("BOTTOMRIGHT", 0, 2);
+
+    btn.timeRemainingText = btn:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall");
+    btn.timeRemainingText:SetPoint("TOP", btn, "BOTTOM", 0, -2);
+
+    return btn;
 end
 
-function C_Aura:Disable(data)
-    data.frame:EnableMouse(false);
-    data.frame.iconTexture:SetTexture("");
-    data.frame.background:SetAlpha(0);
+local function SetAuraButtonIconTexture(btn, iconTexture)
+    if (iconTexture) then
+        btn:EnableMouse(true);
+        btn.iconTexture:SetTexture(iconTexture);
+        btn.forceUpdate = true;
+        btn:SetAlpha(1);
+    else
+        btn:EnableMouse(false);
+        btn.iconTexture:SetTexture("");
+        btn:SetAlpha(0);
+    end
+end
+
+---@param auras table
+---@param totalAuras number
+---@param filter string
+local function AuraArea_OnEvent(_, _, buttons, totalAuras, filter)
+    local totalArgs = select("#", GetWeaponEnchantInfo());
+    local totalItems = totalArgs / ARGS_PER_ITEM;
+    local totalActiveEnchants = 0;
+
+    -- check weapon enchant auras:
+    for index = 1, totalItems do
+        local hasEnchant = select(ARGS_PER_ITEM * (index - 1) + 1, GetWeaponEnchantInfo());
+        local btn = buttons[index];
+        local iconTexture;
+        btn.id = enchantAuraIds[index];
+        btn:SetAttribute("type2", nil);
+        btn:SetAttribute("cancelaura", nil);
+
+        if (hasEnchant) then
+            btn.filter = nil; -- for tooltip to work
+            iconTexture = GetInventoryItemTexture("player", enchantAuraIds[index]);
+            totalActiveEnchants = totalActiveEnchants + 1;
+        end
+
+        SetAuraButtonIconTexture(btn, iconTexture);
+    end
+
+    -- check regular auras:
+    for auraID = 1, totalAuras - #enchantAuraIds do
+        local buttonIndex = auraID + totalActiveEnchants;
+        local btn = buttons[buttonIndex];
+        local name, iconTexture = UnitAura("player", auraID, filter);
+        btn.id = auraID;
+        btn:SetAttribute("type2", "cancelaura");
+        btn:SetAttribute("cancelaura", "player", auraID);
+        SetAuraButtonIconTexture(btn, name and iconTexture);
+    end
+end
+
+-- should only handle updating time remaining and counts
+local function AuraArea_OnUpdate(self, elapsed, auraButtons)
+    self.timeSinceLastUpdate = self.timeSinceLastUpdate + elapsed;
+
+    for _, btn in ipairs(auraButtons) do
+        if (self.timeSinceLastUpdate > 1 or btn.forceUpdate) then
+            btn.forceUpdate = nil;
+
+            if (tk.Tables:Contains(enchantAuraIds, btn.id)) then
+                AuraEnchantButton_OnUpdate(btn, self:GetName());
+            else
+                AuraButton_OnUpdate(btn);
+            end
+
+            if (self.timeSinceLastUpdate > 1) then
+                self.timeSinceLastUpdate = 0;
+            end
+        end
+    end
 end
 
 -- C_AuraArea ----------------------
@@ -229,9 +235,9 @@ end
 Engine:DefineParams("table", "string");
 function C_AuraArea:__Construct(data, settings, areaName)
     data.settings = settings[areaName];
+    data.appearance = settings.appearance; -- not using yet
     data.globalName = string.format("MUI_%sArea", areaName);
-    data.appearance = settings.appearance;
-    data.auras = obj:PopTable();
+    data.auraButtons = obj:PopTable(); -- stores all C_Aura objects (including enchant auras)
 
     if (data.settings.auraType == "Buffs") then
         data.totalAuras = BUFF_MAX_DISPLAY;
@@ -242,140 +248,78 @@ function C_AuraArea:__Construct(data, settings, areaName)
     end
 end
 
-do
-    ---@param auras table
-    ---@param totalAuras number
-    ---@param filter string
-    local function CheckAuras(_, _, auras, totalAuras, filter)
-        -- need to check mainHandEnchant and offHandEnchant
-
-
-        for index = 1, totalAuras do
-            local aura = auras[index]; ---@type C_Aura
-            local name, iconTexture = UnitAura("player", index, filter);
-
-            if (name) then
-                aura:Enable(iconTexture, false);
-            else
-                aura:Disable();
-            end
-        end
+Engine:DefineParams("boolean")
+function C_AuraArea:SetEnabled(data, enabled)
+    if (not data.frame and not enabled) then
+        return;
     end
 
-    function C_AuraArea:CreateAuraAreaFrame(data)
-        data.frame = CreateFrame("Frame", data.globalName, UIParent);
-        framesToUpdate[tostring(data.frame)] = obj:PopTable();
+    if (enabled) then
+        if (not data.frame) then
+            data.frame = CreateFrame("Frame", data.globalName, UIParent);
 
-        local s = data.settings;
-        data.frame:SetSize(
-            ((s.auraSize + s.colSpacing) * s.perRow) - s.colSpacing,
-            ((s.auraSize + s.rowSpacing) * math.ceil(BUFF_MAX_DISPLAY / s.perRow)) - s.rowSpacing);
+            local s = data.settings;
+            data.frame:SetSize(
+                ((s.auraSize + s.colSpacing) * s.perRow) - s.colSpacing,
+                ((s.auraSize + s.rowSpacing) * math.ceil(BUFF_MAX_DISPLAY / s.perRow)) - s.rowSpacing);
 
-        data.frame:SetPoint(unpack(s.position));
+            data.frame:SetPoint(unpack(s.position));
 
-        if (data.settings.showEnchants) then
-            data.mainHandAura = C_Aura(self, MAIN_ENCHANT_ID);
-            data.offHandAura = C_Aura(self, OFF_ENCHANT_ID);
-        end
-
-        for index = 1, data.totalAuras do
-            data.auras[index] = C_Aura(self, index);
-        end
-
-        self:PositionAuraFrames();
-
-        -- Can I shorten this?
-        em:CreateUnitEventHandlerWithKey("UNIT_AURA", data.globalName.."Handler", CheckAuras, "Player")
-            :SetCallbackArgs(data.auras, data.totalAuras, data.filter)
-            :AppendEvent("GROUP_ROSTER_UPDATE")
-            :AppendEvent("PLAYER_ENTERING_WORLD");
-    end
-end
-
-do
-    -- should only handle updating time remaining and counts
-    local function AuraArea_OnUpdate(self, elapsed)
-        self.timeSinceLastUpdate = self.timeSinceLastUpdate + elapsed;
-
-        if (self.timeSinceLastUpdate < 1) then
-            return;
-        end
-
-        self.timeSinceLastUpdate = 0;
-
-        for _, auraFrame in ipairs(framesToUpdate[tostring(self)]) do
-            local id = auraFrame:GetID();
-
-            if (id == MAIN_ENCHANT_ID or id == OFF_ENCHANT_ID) then
-                AuraEnchantFrame_OnUpdate(auraFrame, self:GetName());
-            else
-                AuraFrame_OnUpdate(auraFrame);
-            end
-        end
-    end
-
-    Engine:DefineParams("boolean")
-    function C_AuraArea:SetEnabled(data, enabled)
-        if (not data.frame and not enabled) then
-            return;
-        end
-
-        if (enabled) then
-            if (not data.frame) then
-                self:CreateAuraAreaFrame();
+            for index = 1, (data.totalAuras + #enchantAuraIds) do
+                data.auraButtons[index] = CreateAuraButton(data.frame, data.filter);
             end
 
-            data.frame.timeSinceLastUpdate = 0;
-            data.frame:SetScript("OnUpdate", AuraArea_OnUpdate);
+            self:PositionAuraButtons();
 
-            -- Hide Blizzard frames
-            tk:KillElement(_G.BuffFrame);
-            tk:KillElement(_G.TemporaryEnchantFrame);
-        else
-            data.frame:SetScript("OnUpdate", nil);
-            em:DestroyEventHandlerByKey(data.globalName.."Handler");
-            data.frame:SetParent(tk.Constants.DUMMY_FRAME);
-            data.frame:SetAllPoints(tk.Constants.DUMMY_FRAME);
+            -- Can I shorten this?
+            em:CreateUnitEventHandlerWithKey("UNIT_AURA", data.globalName.."Handler", AuraArea_OnEvent, "Player")
+                :SetCallbackArgs(data.auraButtons, data.totalAuras, data.filter)
+                :AppendEvent("GROUP_ROSTER_UPDATE")
+                :AppendEvent("PLAYER_ENTERING_WORLD")
         end
 
-        data.frame:SetShown(enabled);
-        data.enabled = enabled;
+        data.frame.timeSinceLastUpdate = 0;
+        data.frame:SetScript("OnUpdate", function(self, elapsed)
+            AuraArea_OnUpdate(self, elapsed, data.auraButtons);
+        end);
+
+        -- Hide Blizzard frames
+        tk:KillElement(_G.BuffFrame);
+        tk:KillElement(_G.TemporaryEnchantFrame);
+    else
+        data.frame:SetScript("OnUpdate", nil);
+        em:DestroyEventHandlerByKey(data.globalName.."Handler");
+        data.frame:SetParent(tk.Constants.DUMMY_FRAME);
+        data.frame:SetAllPoints(tk.Constants.DUMMY_FRAME);
     end
+
+    data.frame:SetShown(enabled);
+    data.enabled = enabled;
 end
 
 Engine:SetAttribute("Framework.System.Attributes.InCombatAttribute");
-function C_AuraArea:PositionAuraFrames(data)
-    local frames = obj:PopTable();
-
-    for _, aura in pairs(data.auras) do
-        local frame = aura:GetFrame();
-        obj:Assert(obj:IsWidget(frame, "Frame"), "Frame is missing.");
-        frame:ClearAllPoints();
-        table.insert(frames, frame);
-    end
-
+function C_AuraArea:PositionAuraButtons(data)
     local totalPositioned = 0;
 
-    for id, auraFrame in ipairs(frames) do
+    for id, auraBtn in ipairs(data.auraButtons) do
         if (id == 1) then
             if (data.settings.direction == "LEFT") then
-                auraFrame:SetPoint("TOPRIGHT");
+                auraBtn:SetPoint("TOPRIGHT");
             elseif (data.settings.direction == "RIGHT") then
-                auraFrame:SetPoint("TOPLEFT");
+                auraBtn:SetPoint("TOPLEFT");
             end
 
         elseif (totalPositioned % data.settings.perRow == 0) then
-            local anchor = frames[(totalPositioned - data.settings.perRow) + 1];
-            auraFrame:SetPoint("TOP", anchor, "BOTTOM", 0, -data.settings.rowSpacing);
+            local anchor = data.auraButtons[(totalPositioned - data.settings.perRow) + 1];
+            auraBtn:SetPoint("TOP", anchor, "BOTTOM", 0, -data.settings.rowSpacing);
         else
             if (data.settings.direction == "LEFT") then
-                auraFrame:SetPoint("RIGHT", frames[id - 1], "LEFT", -data.settings.colSpacing, 0);
+                auraBtn:SetPoint("RIGHT", data.auraButtons[id - 1], "LEFT", -data.settings.colSpacing, 0);
             elseif (data.settings.direction == "RIGHT") then
-                auraFrame:SetPoint("LEFT", frames[id - 1], "RIGHT", data.settings.colSpacing, 0);
+                auraBtn:SetPoint("LEFT", data.auraButtons[id - 1], "RIGHT", data.settings.colSpacing, 0);
             end
         end
 
-        auraFrame:Show();
         totalPositioned = totalPositioned + 1;
     end
 end
